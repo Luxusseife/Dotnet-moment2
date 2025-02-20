c593751f59cc511e6c7b06a0106d238b538f4644
Jenny Lind <jeli2308@student.miun.se>
2025-02-20 14:59:58 +0100

    Tömt json-filen, förberett indentering av json-data med statisk variabel. I POST-anropet har inläsning av json-filen lagts till, generering av ett ID görs och bok kan lagras.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -23,23 +23,50 @@ public class HomeController : Controller
         return View();
     }
 
+    // Sätter en statisk variabel som hanterar JSON-indentering i den interna JSON-filen. 
+    private static readonly JsonSerializerOptions jsonOptions = new() { WriteIndented = true };
+
     // Hantering av POST-anrop.
     [HttpPost]
+    [Route("/add")]
     public IActionResult Add(BookModel model)
     {
         // Sätter aktuellt år i ViewBag.
         var currentYear = DateTime.Now.Year;
         ViewBag.CurrentYear = currentYear;
 
-        // Validerar formulär.
+        // Validerar formuläret, läser in json-fil och lagrar ny data.
         if (ModelState.IsValid)
         {
-            return View();
+            // Läser in myBooks.json-filen.
+            string jsonStr = System.IO.File.ReadAllText("myBooks.json");
+
+            // Deserialiserar JSON.
+            var books = JsonSerializer.Deserialize<List<BookModel>>(jsonStr, jsonOptions) ?? new List<BookModel>();
+
+            // Genererar ett nytt ID. Börjar på 1 om listan är tom, annars läggs +1 på varje gång en bok läggs till.
+            model.Id = books.Count > 0 ? books.Max(b => b.Id) + 1 : 1;
+
+            // Lägger till en ny kurs.
+            if (books != null)
+            {
+                books.Add(model);
+
+                // Serialiserar JSON.
+                jsonStr = JsonSerializer.Serialize(books, jsonOptions);
+
+                // Skriver in/över ändringarna till myBooks.json-filen.
+                System.IO.File.WriteAllText("myBooks.json", jsonStr);
+            }
+
+            // Återställer formuläret.
+            ModelState.Clear();
+
+            // Omdirigerar användaren till "My books", tabellen med böcker.
+            return RedirectToAction("Books", "Home");
         }
-        else
-        {
-            return View();
-        }
+
+        return View();
     }
 
     [Route("/books")]
